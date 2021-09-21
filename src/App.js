8184b897e30477f56525dde1a9155deaad5a40e4
jbro129 <jbro129fan@gmail.com>
2021-09-21 01:25:00 -0400

    Added portfolio card instead of link in footer and added flex wrap to projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Badge } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css'
 
 import skillsJson from './components/skills.json'
@@ -9,7 +10,7 @@ import ProjectCard from './components/ProjectCard'
 
 import TMIcon from './assets/icon_tm.webp'
 import JbroIcon from './assets/icon_jbro129.webp'
-import Footer from './components/Footer'
+import HeadshotIcon from './assets/headshot.webp'
 
 function addSkills() {
 	return skillsJson.map((skill) => {
@@ -40,7 +41,7 @@ function App() {
 							button1txt="Source"
 							button1click={() => {
 								alert(
-									'The TManager Android App source is private. If you are a recruiter and are interested in seeing the code then please connect with me on Linkedin and send me a message'
+									'The TManager Android App source is private. If you are interested in seeing the code then please connect with me on LinkedIn and send me a message'
 								)
 							}}
 							button2txt="Google Play Store"
@@ -58,7 +59,7 @@ function App() {
 							button1txt="Source"
 							button1click={() => {
 								alert(
-									'The TManager Web App source is private. If you are a recruiter and are interested in seeing the code then please connect with me on Linkedin and send me a message'
+									'The TManager Web App source is private. If you interested in seeing the code then please connect with me on LinkedIn and send me a message'
 								)
 							}}
 							button2txt="Deployed"
@@ -73,6 +74,21 @@ function App() {
 							button1txt="YouTube Channel"
 							button1click={() => {
 								window.open('https://www.youtube.com/Jbro129', '_blank')
+							}}
+						/>
+						<ProjectCard
+							title="Jonahs Portfolio"
+							desc="This is my portfolio that I built in React.js with Bootstrap and deployed with Firebase."
+							image={HeadshotIcon}
+							button1txt="Source"
+							button1click={() => {
+								window.open('https://github.com/jbro129/portfolio', '_blank')
+							}}
+							button2txt="Deployed"
+							button2click={() => {
+								alert(
+									'You are already here! :)'
+								)
 							}}
 						/>
 					</div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ function Footer() {
 	return (
 		<>
 			<div className="footer-container">
-				<h5 style={{ color: 'black' }}>
+				{/* <h5 style={{ color: 'black' }}>
 					<a href="https://github.com/jbro129/portfolio" target="_blank">
 						Click here to visit the Github project for this portfolio
 					</a>
-				</h5>
+				</h5> */}
 			</div>
 		</>
 	)

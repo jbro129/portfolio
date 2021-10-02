56f6d913bcb481008c24748373c693d9d531a315
jbro129 <jbro129fan@gmail.com>
2021-10-02 16:20:24 -0400

    Removed testing and fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-const rootElement = document.getElementById('root')
-
-if (rootElement.hasChildNodes()) {
-	ReactDOM.hydrate(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-		rootElement
-	)
-} else {
-	ReactDOM.render(
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>,
-		rootElement
-	)
-}
-
-reportWebVitals()
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

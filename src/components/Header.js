0031ad8c7fcd76a24fbfec1cec342a44162e65d8
jbro129 <jbro129fan@gmail.com>
2022-03-15 21:40:53 -0400

    Added more skills and updated resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -51,7 +51,7 @@ function Header() {
 				<div
 					className="social-button"
 					onClick={() => {
-						window.open("/assets/Jonah Allen - Resume.pdf", '_blank')
+						window.open("https://storage.googleapis.com/storage-jsa/Jonah%20Allen%20-%20Resume.pdf", '_blank')
 					}}
 				>
 					<img
